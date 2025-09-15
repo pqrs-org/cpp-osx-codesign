5b28ce44d4a9e2e0b6f17f3102d488c613f1d3f7
Takayama Fumihiko <tekezo@pqrs.org>
2025-09-15 22:06:09 +0900

    Update tests

--- tests/src/test.cpp
+++ tests/src/test.cpp
@@ -4,9 +4,19 @@
 int main(void) {
   using namespace boost::ut;
   using namespace boost::ut::literals;
+  using namespace std::literals;
+
+  "get_signing_information_of_process"_test = [] {
+    auto actual = pqrs::osx::codesign::get_signing_information_of_process(1);
+    expect(actual.get_team_id() == std::nullopt);
+    expect(actual.get_identifier() != std::nullopt);
+    expect("com.apple.xpc.launchd"sv == *(actual.get_identifier()));
+  };
 
   "find_common_name_of_process"_test = [] {
-    expect(pqrs::osx::codesign::find_common_name_of_process(1) != std::nullopt);
+    auto actual = pqrs::osx::codesign::find_common_name_of_process(1);
+    expect(actual != std::nullopt);
+    expect("Software Signing"sv == *actual);
   };
 
   return 0;
