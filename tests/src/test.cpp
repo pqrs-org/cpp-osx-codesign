3839f54045826f8f28f479fc76d3d14910c569b7
Takayama Fumihiko <tekezo@pqrs.org>
2026-01-10 21:25:21 +0900

    Update tests

--- tests/src/test.cpp
+++ tests/src/test.cpp
@@ -8,9 +8,31 @@ int main(void) {
 
   "get_signing_information_of_process"_test = [] {
     auto actual = pqrs::osx::codesign::get_signing_information_of_process(1);
-    expect(actual.get_team_id() == std::nullopt);
-    expect(actual.get_identifier() != std::nullopt);
+    expect(std::nullopt == actual.get_team_id());
     expect("com.apple.xpc.launchd"sv == *(actual.get_identifier()));
+  };
+
+  "get_signing_information_of_file"_test = [] {
+    {
+      auto actual = pqrs::osx::codesign::get_signing_information_of_file("data/apps/Applications/spoofed-identifier.app");
+      expect("G43BCU2T37"sv == actual.get_team_id());
+      expect("com.apple.dummy"sv == actual.get_identifier());
+    }
+    {
+      auto actual = pqrs::osx::codesign::get_signing_information_of_file("data/apps/Applications/unsigned.app");
+      expect(std::nullopt == actual.get_team_id());
+      expect(std::nullopt == actual.get_identifier());
+    }
+    {
+      auto actual = pqrs::osx::codesign::get_signing_information_of_file("data/apps/Applications/valid-signature.app");
+      expect("G43BCU2T37"sv == actual.get_team_id());
+      expect("org.pqrs.cpp-osx-codesign.valid-signature"sv == actual.get_identifier());
+    }
+    {
+      auto actual = pqrs::osx::codesign::get_signing_information_of_file("/System/Applications/Utilities/Terminal.app");
+      expect(std::nullopt == actual.get_team_id());
+      expect("com.apple.Terminal"sv == actual.get_identifier());
+    }
   };
 
   "find_common_name_of_process"_test = [] {
