0086d0f4f4c7dd79da28c6c952983a3e24f14c29
Takayama Fumihiko <tekezo@pqrs.org>
2026-06-21 21:14:36 +0900

    Use adopt_cf_ptr

--- tests/src/test.cpp
+++ tests/src/test.cpp
@@ -1,7 +1,7 @@
 #include <boost/ut.hpp>
 #include <pqrs/osx/codesign.hpp>
 
-int main(void) {
+int main() {
   using namespace boost::ut;
   using namespace boost::ut::literals;
   using namespace std::literals;
